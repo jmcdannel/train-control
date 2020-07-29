18db2bf23602b233e7427bc7353028fea29a1220
Josh McDannel <mcdannel@gmail.com>
2020-07-29 11:43:09 -0600

    Updates

---
+++ precache-manifest.3aab8b5234dbbeb9838e1ab691aabf2f.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6970f56061f63715140fce8ef67bef27",
+    "url": "/github.io/train-control/index.html"
+  },
+  {
+    "revision": "fc56eedf56154f6aa8e7",
+    "url": "/github.io/train-control/static/css/main.baabd080.chunk.css"
+  },
+  {
+    "revision": "616ef42635d89f3a32f7",
+    "url": "/github.io/train-control/static/js/2.3096a0dd.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/github.io/train-control/static/js/2.3096a0dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc56eedf56154f6aa8e7",
+    "url": "/github.io/train-control/static/js/main.9f41e035.chunk.js"
+  },
+  {
+    "revision": "c02424a26641466a1160",
+    "url": "/github.io/train-control/static/js/runtime-main.92db74bc.js"
+  },
+  {
+    "revision": "9c3d904abedd6a4b28f461638cabd248",
+    "url": "/github.io/train-control/static/media/IDAWANY-base.9c3d904a.png"
+  },
+  {
+    "revision": "e0160c422c0ee5aba5500618deddaedf",
+    "url": "/github.io/train-control/static/media/IDAWANY-blueline.e0160c42.png"
+  },
+  {
+    "revision": "9b2398985eb3cb5a7b52fe3a47e51ef4",
+    "url": "/github.io/train-control/static/media/IDAWANY-brownline.9b239898.png"
+  },
+  {
+    "revision": "deb95cb4f6493d8ffba4eefd30fb3b14",
+    "url": "/github.io/train-control/static/media/IDAWANY-greenline.deb95cb4.png"
+  },
+  {
+    "revision": "90f049870de3fde751ee7c8e805adecf",
+    "url": "/github.io/train-control/static/media/IDAWANY-magentaline.90f04987.png"
+  },
+  {
+    "revision": "6ff3ae42bd4ad188e03fdbddd705913f",
+    "url": "/github.io/train-control/static/media/IDAWANY-orangeline.6ff3ae42.png"
+  },
+  {
+    "revision": "57038a140971b0c630e04a34befe0710",
+    "url": "/github.io/train-control/static/media/IDAWANY-purpleline.57038a14.png"
+  },
+  {
+    "revision": "8c26ca8a014f06ab20ccbe0c19210af0",
+    "url": "/github.io/train-control/static/media/IDAWANY-redline.8c26ca8a.png"
+  },
+  {
+    "revision": "049c4d96c182be72b15cd5fed53746ab",
+    "url": "/github.io/train-control/static/media/IDAWANY-switches.049c4d96.png"
+  },
+  {
+    "revision": "90eb524b6c6f98c10e1b650fe62ff638",
+    "url": "/github.io/train-control/static/media/IDAWANY-yellowline.90eb524b.png"
+  },
+  {
+    "revision": "e1411ea7c95ba5a7633ef880e192f4f9",
+    "url": "/github.io/train-control/static/media/switch.e1411ea7.svg"
+  }
+]);
