a6e576218226cac589865aa80af5e50644b2d6b4
jmcdannel <mcdannel@gmail.com>
2020-08-27 15:53:01 +0000

    modify ssl config

--- api/api.py
+++ api/api.py
@@ -1,12 +1,19 @@
 import os
+import logging
+import RPi.GPIO as GPIO
 from flask_cors import CORS
 from flask import Flask, render_template, url_for, json, jsonify, request, abort
 from adafruit_servokit import ServoKit
 
 app = Flask(__name__)
+logging.getLogger('flask_cors').level = logging.DEBUG
 app.config['DEBUG'] = True
 CORS(app)
-
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(22, GPIO.OUT)
+GPIO.setup(27, GPIO.IN)
+GPIO.setup(21, GPIO.OUT)
 kit = ServoKit(channels=16)
 
 path = os.path.dirname(__file__) + '/turnouts.json'
@@ -26,17 +33,19 @@ def get_turnout(turnout_id):
   with open(path) as turnout_file:
         data = json.load(turnout_file)
   turnout = [turnout for turnout in data if turnout['id'] == turnout_id]
+  
   if len(turnout) == 0:
       abort(404)
-  return jsonify(turnout)
+  return jsonify(turnout[0])
 
 @app.route('/turnouts/<int:turnout_id>', methods=['PUT'])
 def update_turnout(turnout_id):
   with open(path) as turnout_file:
         data = json.load(turnout_file)
-  turnout = [turnout for turnout in data if turnout['id'] == turnout_id]
-  if len(turnout) == 0:
+  turnouts = [turnout for turnout in data if turnout['id'] == turnout_id]
+  if len(turnouts) == 0:
       abort(404)
+  turnout = turnouts[0]
   if not request.json:
       abort(400)
   if 'current' in request.json and type(request.json['current']) is not int:
@@ -45,13 +54,26 @@ def update_turnout(turnout_id):
       abort(400)
   if 'divergent' in request.json and type(request.json['divergent']) is not int:
       abort(400)
-  kit.servo[turnout[0]['servo']].angle = turnout[0]['current']
+
+  # Turn servo to current degrees
+  if 'servo' in turnout:
+    kit.servo[turnout['servo']].angle = turnout['current']
+
+  # Toggle relay if present
+  if 'relay' in turnout:
+    if turnout['current'] == turnout['straight']:
+      GPIO.output(turnout['relay'], False)
+    else:
+      GPIO.output(turnout['relay'], True)
+
+  # save all keys
   for key in request.json:
-    turnout[0][key] = request.json.get(key, turnout[0][key])
+    turnout[key] = request.json.get(key, turnout[key])
   with open(path, 'w') as turnout_file:
         json.dump(data, turnout_file)
 
-  return jsonify(turnout[0])
+  return jsonify(turnout)
 
 if __name__ == '__main__':
-    app.run(ssl_context='adhoc', host='0.0.0.0')
+    context = ('server.crt', 'server.key')#certificate and key files
+    app.run(host='0.0.0.0', ssl_context=context)
