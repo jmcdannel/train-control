841a156c4ef7dfcb816eaa5abb2469ca420f451d
Josh McDannel <mcdannel@gmail.com>
2021-01-09 17:18:20 -0700

    dev

--- api/effects/solidstrip.py
+++ api/effects/solidstrip.py
@@ -10,6 +10,8 @@ import time
 from rpi_ws281x import *
 import argparse
 
+LED_COLOR       = Color(255, 0, 0)
+
 # LED strip configuration:
 LED_COUNT      = 12      # Number of LED pixels.
 LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
@@ -22,21 +24,10 @@ LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
 # Main program logic follows:
 if __name__ == '__main__':
-    # Process arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-c', '--clear', action='store_true', help='clear the display on exit')
-    args = parser.parse_args()
-
-    # Create NeoPixel object with appropriate configuration.
     strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
-    # Intialize the library (must be called once before other functions).
     strip.begin()
 
-    print ('Press Ctrl-C to quit.')
-    if not args.clear:
-        print('Use "-c" argument to clear LEDs on exit')
-
     for i in range(strip.numPixels()):
-        strip.setPixelColor(i, Color(255, 235, 0))
+        strip.setPixelColor(i, LED_COLOR)
         strip.show()
 
