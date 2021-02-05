1c58132ddd24e750613f0f028dc86c2e1bfb8c88
Josh McDannel <joshmcdannel@7summitsinc.com>
2021-02-05 16:19:17 -0700

    debug

--- api/turnouts/turnoutsapi.py
+++ api/turnouts/turnoutsapi.py
@@ -11,7 +11,7 @@ GPIO = None
 if appConfig['turnouts']['device'] == 'pi':
   try:
     import RPi.GPIO as GPIO
-    GPIO.setmode(GPIO.BOARD)
+    GPIO.setmode(GPIO.BCM)
     print('GPIO Mode set to BOARD')
   except ImportError as error:
     # Output expected ImportErrors.
